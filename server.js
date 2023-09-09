34dd52e8764e92d72fbba178bba502240b86d440
Priyanshu <priyanshukanji0@gmail.com>
2023-09-09 11:31:28 +0530

    Server Crashing fixed

--- server.js
+++ server.js
@@ -1,51 +1,42 @@
 const express = require("express");
 const app = express();
-const mongoose=require('mongoose');
-const path = require('path');
-const homeRouter = require('./routes/articles');
-const publicpath = path.join(__dirname, 'public');
-mongoose.connect('mongodb://localhost:27017/Blogs',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-app.use(express.urlencoded({extended:false})=>{
-    title:req.body.title,
-    description:req.body.description,
-    write:req.body.write-here
-})
+const mongoose = require("mongoose");
+const path = require("path");
+const homeRouter = require("./routes/articles");
+const publicpath = path.join(__dirname, "public");
 app.use(express.static(publicpath));
-app.set('view engine', 'ejs');
-;
-app.get('/', (req, res) => {
-    res.render('Home');
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("Home");
 });
-app.get('/login', (req, res) => {
-    res.render('login')
+app.get("/login", (req, res) => {
+  res.render("login");
 });
-app.get('/signup', (req, res) => {
-    res.render('signup')
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
-app.get('/blog', (req, res) => {
-    const blogs = [{
-        title: 'test title',
-        Date: new Date(),
-        Description: 'test description'
+app.get("/blog", (req, res) => {
+  const blogs = [
+    {
+      title: "test title",
+      Date: new Date(),
+      Description: "test description",
     },
     {
-        title: 'test title1',
-        Date: new Date(),
-        Description: 'test description6'
-    }
-    ]
-    res.render('blog', {
-        blogs: blogs
-    });
+      title: "test title1",
+      Date: new Date(),
+      Description: "test description6",
+    },
+  ];
+  res.render("blog", {
+    blogs: blogs,
+  });
 });
-app.get('/blogs/new',(req,res)=>{
-    res.render('new')
+app.get("/blogs/new", (req, res) => {
+  res.render("new");
 });
-app.use('/Home', homeRouter);
+app.use("/Home", homeRouter);
 
 app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+  console.log("Server is running on port 3001");
 });
